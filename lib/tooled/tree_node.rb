546af6dfe63785c8b2e3a8d49e8dc5ae479f259e
Darren Coxall <darren.coxall@simplybusiness.co.uk>
2013-06-26 09:52:16 +0100

    Added ancestors and descendants to TreeNode
    TreeNodes can now lazily report their descendants and ancestors using enumerators

--- lib/tooled/tree_node.rb
+++ lib/tooled/tree_node.rb
@@ -4,19 +4,44 @@ module Tooled
   class TreeNode
     def initialize
       @children = TreeNodeCollection.new
-      @parent = nil
+      parent = nil
     end
 
     def children
       @children
     end
 
+    def each_child
+      return to_enum(:each_child) unless block_given?
+      children.each do |child|
+        yield child
+        child.each_child { |c| yield c }
+      end
+    end
+
     def parent
       @parent
     end
 
+    def descendants
+      enum = to_enum(:each_child)
+      TreeNodeCollection.new(enum.respond_to?(:lazy) ? enum.lazy : enum)
+    end
+
+    def ancestors
+      enum = ::Enumerator.new do |yielder|
+        parent_node = parent
+        while parent_node
+          yielder << parent_node
+          parent_node = parent_node.parent
+        end
+      end
+      TreeNodeCollection.new(enum.respond_to?(:lazy) ? enum.lazy : enum)
+    end
+
     def parent=(node)
-      raise InvalidNodeException.new("`parent' can only sbe a TreeNode") unless node.is_a?(TreeNode)
+      raise InvalidNodeException.new("`parent' can only be a TreeNode") unless node.is_a?(TreeNode)
+      raise InvalidNodeException.new("`parent' can not be a descendant") if descendants.member?(node)
       @parent = node.tap { |n| n.children.add(self) }
     end
   end
--- spec/tree_node_spec.rb
+++ spec/tree_node_spec.rb
@@ -6,6 +6,40 @@ describe Tooled::TreeNode do
 
   its(:children) { should be_instance_of(Tooled::TreeNodeCollection) }
 
+  describe "reports all descendants" do
+    subject { object.descendants }
+
+    before do
+      object.children << Tooled::TreeNode.new
+      object.children << Tooled::TreeNode.new
+      object.children << Tooled::TreeNode.new.tap do |node_a|
+        node_a.children << Tooled::TreeNode.new.tap do |node_b|
+          node_b.children << Tooled::TreeNode.new
+        end
+      end
+    end
+
+    it { should be_instance_of(Tooled::TreeNodeCollection) }
+    its(:size) { should eql(5) }
+  end
+
+  describe "reports all ancestors" do
+    subject { object.ancestors }
+
+    before do
+      object.parent = Tooled::TreeNode.new.tap do |node_a|
+        node_a.parent = Tooled::TreeNode.new.tap do |node_b|
+          node_b.parent = Tooled::TreeNode.new.tap do |node_c|
+            node_c.parent = Tooled::TreeNode.new
+          end
+        end
+      end
+    end
+
+    it { should be_instance_of(Tooled::TreeNodeCollection) }
+    its(:size) { should eql(4) }
+  end
+
   describe "setting the parent" do
     context "to an invalid object" do
       specify { expect { object.parent = String.new }.to raise_error(Tooled::InvalidNodeException) }
@@ -21,7 +55,12 @@ describe Tooled::TreeNode do
 
       its(:parent) { should eql(parent) }
     end
-  end
 
-  it "can be traversed"
+    context "to a child" do
+      let(:child) { Tooled::TreeNode.new }
+      before { object.children.add(child) }
+
+      specify { expect { object.parent = child }.to raise_error(Tooled::InvalidNodeException) }
+    end
+  end
 end
